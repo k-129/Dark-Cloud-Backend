d9f27fc19088dfa5b8a0b4351fcc055ca26e20c9
Erik Knoef <erikknoef1997@hotmail.com>
2023-11-26 18:31:24 +0000

    solving cors

--- server.js
+++ server.js
@@ -2,23 +2,24 @@ const app = require("./app");
 const cors = require("cors");
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Use CORS middleware
+const allowedOrigins = process.env.ORIGIN
+  ? [process.env.ORIGIN]
+  : ["http://localhost:5173", "https://mainbase.pt"];
+
 app.use(
   cors({
-    origin: "http://localhost:5173" || ORIGIN,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
